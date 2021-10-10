b8723d80aebcecfcc323efb336d0a70f30dafaf1
yn4k4ishi <yn4k4nishi.kit@gmail.com>
2021-10-10 19:04:19 +0900

    :+1: Update : plot heatmap

--- problem_generator/main.cpp
+++ problem_generator/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <Eigen/Dense>
+#include "../matplotlib-cpp/matplotlibcpp.h"
+
+namespace plt = matplotlibcpp;
 
 int main(){
-    std::cout << "Hello world" << std::endl;
+    std::cout << "Generate matrix ..." << std::endl;
+
+    int N = 50;
+    Eigen::MatrixXd matrix = Eigen::MatrixXd::Random(N,N);
+    
+    std::cout << matrix.size() << std::endl;
+
+    // Prepare data
+    int ncols = N, nrows = N;
+    std::vector<float> z(ncols * nrows);
+    for (int j=0; j<nrows; ++j) {
+        for (int i=0; i<ncols; ++i) {
+            z.at(ncols * j + i) = std::sin(std::hypot(i - ncols/2, j - nrows/2));
+        }
+    }
+
+    Eigen::MatrixXf f = matrix.cast<float>();
+    const float* fp = f.data();
+
+    plt::title("My matrix");
+    PyObject* pobj;
+    plt::imshow(fp, N, N, 1, {{"cmap","Blues"}}, &pobj);
+    plt::colorbar(pobj);
+    plt::show();
 
     return 0;
 }
